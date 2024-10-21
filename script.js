e2a77a6066baa6a51c1e98cc4fa8e3eddf14adf1
marGo <ms-ka@o2.pl>
2024-10-21 23:00:43 +0200

    bug fixes after branch merge

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const decreaseBtn = document.getElementById("decreaseBtn")
-const resetBtn = document.getElementById("resetBtn")
-const increaseBtn = document.getElementById("increaseBtn")
-const countLabel = document.getElementById("countLabel")
+const decreaseBtn = document.getElementById("decreaseBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const countLabel = document.getElementById("countLabel");
 let count = 0;
 
 increaseBtn.onclick = function(){
-  count++
-  countLabel.textContent = count;
+    count++;
+    countLabel.textContent = count;
 }
 decreaseBtn.onclick = function(){
-  count--
-  countLabel.textContent = count;
+    count--;
+    countLabel.textContent = count;
 }
-resetBtn.onclick = function(){
-  count = 0;
-  countLabel.textContent = count;
+resetBtn.onclick = function () {
+    count = 0;
+    countLabel.textContent = count;
 }
